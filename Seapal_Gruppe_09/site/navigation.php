d4a8422bd503bf201900c501e661bb50b21f9e45
deparlak <deparlak@googlemail.com>
2013-05-21 09:13:05 +0200

    - added navigation.php

---
+++ Seapal_Gruppe_09/site/navigation.php
@@ -0,0 +1,23 @@
+<div class="navbar navbar-inverse navbar-fixed-top">
+    <div class="navbar-inner">
+        <div class="container-fluid">
+            <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="brand" href="#">Seapal</a>
+            <div class="nav-collapse collapse">
+                <ul class="nav">
+                    <li class="active"><a href='index.php'>Home</a></li>
+                    <li><a href='app_waypoint.php'>App</a></li>
+                    <li><a href='userguide.php'>User Guide</a></li>
+                    <li><a href='screenshots.php'>Screenshots</a></li>
+                    <li><a href='about.php'>About</a></li>
+                    <li><a href='contact.php'>Contact</a></li>
+
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
