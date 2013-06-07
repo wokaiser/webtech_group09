a0182f25b46384cf5749dfecf128b7d6990ea130
deparlak <deparlak@googlemail.com>
2013-06-07 20:25:06 +0200

    - added tracking.js

---
+++ Seapal_Gruppe_09/de.htwg.seapal-master/de.htwg.seapal-master/Seapal_php/js/app/map/tracking.js
@@ -0,0 +1,16 @@
+function startTracking() {
+    console.log("Tracking starts.");
+    
+    
+} 
+
+
+function startNewTracking(position, data) {
+    
+    addTrackingPoint(position, data);
+}
+
+
+function addTrackingPoint(position, data) {
+
+}
